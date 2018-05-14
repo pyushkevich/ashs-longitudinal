0108f9ffb5b76c1754f0bf941662721e15712bd5
Paul Yushkevich <pyushkevich@gmail.com>
2018-05-14 05:53:08 -0400

    added resample to isotropic, swapdim, and MRF options

--- ashs_longi.sh
+++ ashs_longi.sh
@@ -14,12 +14,12 @@ MPRAGE_BL=$ASHSBL/mprage.nii.gz
 MPRAGE_FU=$ASHSFU/mprage.nii.gz
 
 # TSE - fine to use chunks
-TSE_BL=$ASHSBL/tse_native_chunk_${SIDE}.nii.gz
-TSE_FU=$ASHSFU/tse_native_chunk_${SIDE}.nii.gz
+TSE_BL_RAW=$ASHSBL/tse_native_chunk_${SIDE}.nii.gz
+TSE_FU_RAW=$ASHSFU/tse_native_chunk_${SIDE}.nii.gz
 
 # T1-T2 matrices
-MAT_T1T2_BL=$ASHSBL/flirt_t2_to_t1/flirt_t2_to_t1.mat
-MAT_T1T2_FU=$ASHSFU/flirt_t2_to_t1/flirt_t2_to_t1.mat
+MAT_T1T2_BL=$ASHSBL/flirt_t2_to_t1/flirt_t2_to_t1_inv.mat
+MAT_T1T2_FU=$ASHSFU/flirt_t2_to_t1/flirt_t2_to_t1_inv.mat
 
 # ASHS segmentation
 ASHS_SEG_BL=$ASHSBL/bootstrap/fusion/lfseg_heur_${SIDE}.nii.gz
@@ -52,21 +52,35 @@ MAT_B_SQRT_INV_IT2=$WORK/B_sqrt_inv_it2.mat
 MAT_Z1_IT2=$WORK/Z1_it2.mat
 MAT_Z2_IT2=$WORK/Z2_it2.mat
 
+TSE_BL=$WORK/tse_bl.nii.gz
+TSE_FU=$WORK/tse_fu.nii.gz
 TSE_BL_HW=$WORK/reslice_tse_bl_to_hw.nii.gz
 TSE_FU_HW=$WORK/reslice_tse_fu_to_hw.nii.gz
 TSE_BL_HW_IT2=$WORK/reslice_tse_bl_to_hw_it2.nii.gz
 TSE_FU_HW_IT2=$WORK/reslice_tse_fu_to_hw_it2.nii.gz
 
+MASK_JLF=$WORK/mask_jlf.nii.gz
+
 # Make work directory
 mkdir -p $WORK
 
+<<'SKIP'
+
+
+SKIP
+
+# Resample the TSE images and organize as RSA
+c3d $TSE_BL_RAW -resample 100x100x500% -swapdim RSA -o $TSE_BL
+c3d $TSE_FU_RAW -resample 100x100x500% -swapdim RSA -o $TSE_FU
+
 # Perform whole-brain T1 registration between the T1 scans without a mask
 greedy -d 3 -a -dof 6 \
-  -i $MPRAGE_BL $MPRAGE_FU -m NCC 4x4x4 -n 40x40x0 \
+  -i $MPRAGE_BL $MPRAGE_FU -m NCC 4x4x4 -n 40x80x40x0 \
   -o $MAT_MPRAGE_BLFU -ia-image-centers
 
 # Create a registration mask from the ASHS output (baseline is ok)
-c3d $ASHS_SEG_BL -thresh 1 inf 1 0 -dilate 1 8x8x0 -o $MASK_BL_NATIVE
+c3d $TSE_BL $ASHS_SEG_BL -thresh 1 inf 1 0 -dilate 1 8x8x0 \
+  -reslice-identity -o $MASK_BL_NATIVE
 
 # Peform mask-based registration between the T2 scans using the T2->T1 transforms
 # from ASHS as the starting point
@@ -98,7 +112,6 @@ c3d $REFSPACE $TSE_BL -reslice-matrix $MAT_Z1 -o $TSE_BL_HW
 c3d $REFSPACE $TSE_FU -reslice-matrix $MAT_Z2 -o $TSE_FU_HW
 c3d $REFSPACE $MASK_BL_NATIVE -int 0 -reslice-matrix $MAT_Z1 -o $MASK_BL_HW
 
-
 # Compute a cropped mask
 c3d $REFSPACE -as R $TSE_BL -thresh -inf inf 1 0 \
   -pad 0x0x1vox 0x0x1vox 0 -dilate 0 1x1x1 -reslice-matrix $MAT_Z1 -popas K1 \
@@ -106,16 +119,14 @@ c3d $REFSPACE -as R $TSE_BL -thresh -inf inf 1 0 \
   -pad 0x0x1vox 0x0x1vox 0 -dilate 0 1x1x1 -reslice-matrix $MAT_Z2 -popas K2 \
   -push K1 -push K2 -times $MASK_BL_HW -times -o $MASK_BL_HW_CROP
 
-
 # Perform a registration between halfway images - to account for residual misreg
 greedy -d 3 -dof 6 \
   -i $TSE_BL_HW $TSE_FU -gm $MASK_BL_HW_CROP \
-  -o $MAT_Y2 -ia $MAT_Z2 -a -m NCC 3x3x0 -n 40x40
+  -o $MAT_Y2 -ia $MAT_Z2 -a -m NCC 4x4x4 -n 40x40
 
 greedy -d 3 -dof 6 \
   -i $TSE_FU_HW $TSE_BL -gm $MASK_BL_HW_CROP \
-  -o $MAT_Y1 -ia $MAT_Z1 -a -m NCC 3x3x0 -n 40x40
-
+  -o $MAT_Y1 -ia $MAT_Z1 -a -m NCC 4x4x4 -n 40x40
 
 # Compute the new Z (native to halfway reference) matrices
 c3d_affine_tool $MAT_Z2 $MAT_Y1 -inv -mult -info -o $MAT_Z2_Y1_INV
@@ -137,7 +148,6 @@ c3d $REFSPACE -as R $TSE_BL -thresh -inf inf 1 0 \
   -push K1 -push K2 -times \
   -push R $MASK_BL_NATIVE -int 0 -reslice-matrix $MAT_Z1_IT2 \
   -times -o $MASK_BL_HW_CROP_IT2
-
 
 # Perform ASHS voting
 mkdir -p $WORK/watlas
@@ -169,6 +179,21 @@ for tid in $(ls $ATLAS/train); do
 done
 
 
+# Compute the mask for LF more carefully, excluding edge regions of both segmentations
+# where there is more uncertainty
+c3d \
+  $REFSPACE -as R $ASHS_SEG_BL -thresh 1 inf 1 0 -as X -dup \
+  -pad 0x0x1 0x0x1 0 -dilate 1 300x300x0 -dilate 0 0x0x1 \
+  -int 0 -reslice-identity -push X \
+  -dilate 1 4x4x0 -times \
+  -reslice-matrix $MAT_Z1_IT2 \
+  -push R $ASHS_SEG_FU -thresh 1 inf 1 0 -as X -dup \
+  -pad 0x0x1 0x0x1 0 -dilate 1 300x300x0 -dilate 0 0x0x1 \
+  -int 0 -reslice-identity -push X \
+  -dilate 1 4x4x0 -times \
+  -reslice-matrix $MAT_Z2_IT2 \
+  -times -o $MASK_JLF
+
 # Directory for the posteriors
 mkdir -p $WORK/lfpost
 
@@ -176,14 +201,54 @@ mkdir -p $WORK/lfpost
 ~/tk/malf/xc64rel/label_fusion 3 \
   -g $WORK/watlas/atlas_*.nii.gz \
   -l $WORK/watlas/atseg_*.nii.gz \
-  -rp 3x3x0 -rs 3x3x1 -M $MASK_BL_HW_CROP_IT2 \
+  -rp 3x3x3 -rs 3x3x3 -M $MASK_JLF \
   -p $WORK/lfpost/post_bl_%03d.nii.gz \
   $TSE_BL_HW_IT2 $WORK/ashsseg_bl_hw.nii.gz
 
 ~/tk/malf/xc64rel/label_fusion 3 \
   -g $WORK/watlas/atlas_*.nii.gz \
   -l $WORK/watlas/atseg_*.nii.gz \
-  -rp 3x3x0 -rs 3x3x1 -M $MASK_BL_HW_CROP_IT2 \
+  -rp 3x3x3 -rs 3x3x3 -M $MASK_JLF \
   -p $WORK/lfpost/post_fu_%03d.nii.gz \
   $TSE_FU_HW_IT2 $WORK/ashsseg_fu_hw.nii.gz
+
+
+# Use MRF to create regularized segmentations
+mkdir -p $WORK/mrf
+for i in 000 001 002 003 004 006 007 008 009 011 012 013; do 
+  
+  # Make sure all the posteriors exist
+  for what in bl fu; do
+    if [[ ! -f $WORK/lfpost/post_${what}_${i}.nii.gz ]]; then
+      c3d $REFSPACE -scale 0 -o $WORK/lfpost/post_${what}_${i}.nii.gz 
+    fi
+  done
+
+  # Create the 4D posteriors
+  c4d $WORK/lfpost/post_bl_$i.nii.gz $WORK/lfpost/post_fu_$i.nii.gz \
+    -tile w -o $WORK/mrf/post_blfu_${i}.nii.gz
+
+done
+
+# Create a 4D mask
+c4d $MASK_JLF -dup -tile w -o $WORK/mrf/mask_mrf.nii.gz
+
+# Perform MRF with different lambdas
+for lam in 0.01 0.02 0.04 0.06 0.08 1.0; do
+
+  lamcode=$(echo $lam | awk '{printf "lam_%04d",1000 * $1}')
+
+  c4d \
+    $WORK/mrf/post_blfu_*.nii.gz $WORK/mrf/mask_mrf.nii.gz -popas M \
+    -foreach -clip 0 1 -push M -replace 0 NaN -times -endfor \
+    -vote-mrf VA $lam -o $WORK/mrf/vote4d_${lamcode}.nii.gz \
+    -replace 1 0 2 1 3 2 4 3 5 4 6 5 7 7 8 8 9 9 10 11 11 12 12 13 \
+    -slice w 0:-1 -oo $WORK/mrf/vote_bl_${lamcode}.nii.gz $WORK/mrf/vote_fu_${lamcode}.nii.gz
+
+done
+
+
+
+
+
 
